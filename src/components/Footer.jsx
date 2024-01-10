06a4895b49d5a072da36e856b77ae45f1963d481
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-01-10 12:54:38 -0300

    create footer basic layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,33 @@ const Footer = () => {
             <div className="container">
                 
                     <div className="footer-content">
-                        <p>&copy; 2024 All Rights Reserved </p>
+
+                        <div className="footer-header">
+                            <div className="title">
+                                <h1>Almeida</h1>
+                            </div>
+                            <div className="footer-social">
+                                <a href="https://www.linkedin.com/in/almeidatl" target='_blank'>
+                                    <img src="src/assets/linkedin-in.svg" alt="" />
+                                </a>
+                                <a href="https://github.com/almeidatl" target='_blank'>
+                                    <img src="src/assets/github-alt.svg" alt="" />
+                                </a>
+                                <a href="https://www.instagram.com/almeidatl" target='_blank'>
+                                    <img src="src/assets/instagram.svg" alt="" />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="footer-main">
+                            <p>&copy; 2024 All Rights Reserved </p>
+                        </div>
+                        <div className="footer-message">
+                            <div className="footer-line"></div>
+                            <p>
+                            "Every great design begins with an even better story." <span className="footer-message-author">- Lorinda Mamo</span>
+                            </p>
+                        </div>
                     </div>
                 
             </div>
--- src/components/small-components/GetInTouch.jsx
+++ src/components/small-components/GetInTouch.jsx
@@ -4,7 +4,8 @@ const GetInTouch = () =>{
     return(
         <div className="call center">
             <form action="">
-                <input className='input-email' type="email" name="email" id="email" placeholder='example@email.com' />
+                <input className='user-input' type="text" name="name" id="name" placeholder='Name' />
+                <input className='user-input' type="email" name="email" id="email" placeholder='example@email.com' />
                 <input className='btn' type="submit" value="Let's get in touch" />
             </form>
         </div>
