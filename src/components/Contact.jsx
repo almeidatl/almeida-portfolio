2e9802406c0c5071913e1aca35c403f230dc6175
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-01-10 15:04:48 -0300

    container fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,10 @@
 import GetInTouch from "./small-components/GetInTouch"
-
+import './Contact.css'
 const Contact = () => {
     return(
         <div className="container">
             <section className="contact-content">
-                <h2 className="title">Leave your info below so I can reach you</h2>
+                <h2 className="title">Leave your info <br /> so I can reach you</h2>
                 <GetInTouch />
             </section>
         </div>
