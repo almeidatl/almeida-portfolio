5a8c063f19a21bc6168163513f6a38499dd23f37
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-01-20 15:50:33 -0300

    Update GetInTouch.jsx

--- src/components/small-components/GetInTouch.jsx
+++ src/components/small-components/GetInTouch.jsx
@@ -3,9 +3,10 @@ import './GetInTouch.css'
 const GetInTouch = () =>{
     return(
         <div className="call center">
-            <form action="">
-                <input className='user-input' type="text" name="name" id="name" placeholder='Name' />
-                <input className='user-input' type="email" name="email" id="email" placeholder='example@email.com' />
+            <form action="https://formsubmit.co/e27adcf3ea2246909ac6d7a0b3ace074" method='POST'>
+                <input className='user-input' type="text" name="name" id="name" placeholder='Full Name' />
+                <input className='user-input' type="text" name="phone" id="phone" placeholder='Phone Number' />
+                <input className='user-input' type="email" name="email" id="email" placeholder='Valid Email' />
                 <input className='btn' type="submit" value="Let's get in touch" />
             </form>
         </div>
