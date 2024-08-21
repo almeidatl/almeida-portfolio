e1a39b40f6290c14ff350662e9e329b152bb1b00
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-08-21 19:34:28 -0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     return(
         <div className="container">
             <section className="contact-content">
-                <h2 className="title" id="contact">Deixa um contato <br /> que entro em contato ;)</h2>
+                <h2 className="title" id="contact">Deixa um contato <br /> que falo contigo ;)</h2>
                 <GetInTouch />
             </section>
         </div>
