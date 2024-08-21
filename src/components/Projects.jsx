047f4086f06d99440df0d8fc100b3c214375ec42
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-08-21 19:26:47 -0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,32 +1,34 @@
-import './Projects.css'
-import ProjectCard from './small-components/ProjectCard.jsx'
+import "./Projects.css";
+import ProjectCard from "./small-components/ProjectCard.jsx";
 
-const Projects = () =>{
-    return(
-        <div className="container">
-            <h2 className="title" id="projects-title">My recent work</h2>
-            <section className="projects-content" >
-                <ProjectCard 
-                projectName='ZALLI' 
-                projectLink = 'https://landing-page-zalli.vercel.app'
-                backgroundImg = '/zalli-card.png'
-                />
-                <ProjectCard 
-                projectName='JD' 
-                projectLink = 'https://jdeletrica.vercel.app/'
-                backgroundImg = '/jd-card.png' 
-                />
-                <ProjectCard 
-                projectName='Card' 
-                projectLink = 'https://product-rev-card.vercel.app/'
-                backgroundImg = '/preview-card.png' 
-                />
-                <ProjectCard 
-                projectName='Dr. NORMAN' 
-                projectLink = 'https://themanwhosaved.netlify.app/' 
-                backgroundImg = '/dr-card.png'
-                />
-                {/* <ProjectCard 
+const Projects = () => {
+  return (
+    <div className="container">
+      <h2 className="title" id="projects-title">
+        My recent work
+      </h2>
+      <section className="projects-content">
+        <ProjectCard
+          projectName="ZALLI"
+          projectLink="https://landing-page-zalli.vercel.app"
+          backgroundImg="/zalli-card.png"
+        />
+        <ProjectCard
+          projectName="JD"
+          projectLink="https://jdeletrica.vercel.app/"
+          backgroundImg="/jd-card.png"
+        />
+        <ProjectCard
+          projectName="Card"
+          projectLink="https://product-rev-card.vercel.app/"
+          backgroundImg="/preview-card.png"
+        />
+        <ProjectCard
+          projectName="Dr. NORMAN"
+          projectLink="https://themanwhosaved.netlify.app/"
+          backgroundImg="/dr-card.png"
+        />
+        {/* <ProjectCard 
                 projectName='JS Doc' 
                 projectLink = 'https://documentation-js.netlify.app/'
                 backgroundImg = '/jsdoc-card.png' 
@@ -36,9 +38,9 @@ const Projects = () =>{
                 projectLink = 'https://fcc-form-survey.netlify.app/'
                 backgroundImg = '/survey-card.png' 
                 /> */}
-            </section>
-        </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
