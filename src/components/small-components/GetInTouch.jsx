217003fd2c4d67a1f11167d44b9434888ff325f9
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-08-21 20:57:40 -0300

    Update GetInTouch.jsx

--- src/components/small-components/GetInTouch.jsx
+++ src/components/small-components/GetInTouch.jsx
@@ -4,10 +4,10 @@ const GetInTouch = () =>{
     return(
         <div className="call center">
             <form action="https://formsubmit.co/e27adcf3ea2246909ac6d7a0b3ace074" method='POST'>
-                <input className='user-input' type="text" name="name" id="name" placeholder='Full Name' />
-                <input className='user-input' type="text" name="phone" id="phone" placeholder='Phone Number' />
-                <input className='user-input' type="email" name="email" id="email" placeholder='Valid Email' />
-                <input className='btn' type="submit" value="Let's get in touch" />
+                <input className='user-input' type="text" name="name" id="name" placeholder='Nome' />
+                <input className='user-input' type="text" name="phone" id="phone" placeholder='(xx)9 9999-9999' />
+                <input className='user-input' type="email" name="email" id="email" placeholder='exemplo@email.com' />
+                <input className='btn' type="submit" value="Enviar" />
             </form>
         </div>
     )
